3df43acd761cf5e0d958ed242b62aaa58c0b498d
Luis Vinicius <luisvinicius0906@gmail.com>
2021-07-18 16:41:15 -0300

    Add username input to create user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,9 @@
 class RegistrationsController < ApplicationController
   def create
     user = User.create!(
-      email: params['user']['email']
-      password: params['user']['password']
+      username: params['user']['username'],
+      email: params['user']['email'],
+      password: params['user']['password'],
       password_confirmation: params['user']['password_confirmation']
     )
     
