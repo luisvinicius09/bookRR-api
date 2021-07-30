21b7560027a7e696d7b35dc9ca03426fcf58bfa3
Luis Vinicius <luisvinicius0906@gmail.com>
2021-07-30 17:41:02 -0300

    Add cors for initial deploy while developing front end

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head], credentials: true
-  end
-
-  allow do
-    origins 'https://bookRR.herokuapp.com'
+    origins '*'
     resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head], credentials: true
   end
 end
